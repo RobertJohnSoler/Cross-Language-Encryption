97834a146ff4460b55e3f1bba1e2384d0ce7c6c6
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-04-22 23:33:24 -0500

    started on dll file for go DH client

---
+++ DH_client/client_keygen_dll.go
@@ -0,0 +1,20 @@
+package main
+
+import "C"
+import (
+	"fmt"
+	"net"
+)
+
+func generateKey() *C.char {
+	fmt.Println("Starting connection...")
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	client := NewClient(conn, 128)
+	client.keyAgreementClient()
+	fmt.Println("Agreed key is ", client.key.String())
+	return C.CString(client.key.String())
+}
