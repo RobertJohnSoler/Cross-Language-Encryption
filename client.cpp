6782c33b6c38d2bd36e688921a29d6312675d55e
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-04-25 00:07:52 -0500

    changed client.c to cpp file and compiled it

--- client.cpp
+++ client.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "client_keygen.h"
 
+
 int main(){
     char* key = generateKey();
     printf("Shared key is %s. \n", key);
     return 0;
 }
-
-// need to account for cpp go file
