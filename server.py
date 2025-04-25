a6a645e2056fdc646983d62307b2f8056b5a52d4
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-04-24 23:33:25 -0500

    updated server.py

--- server.py
+++ server.py
@@ -1,1 +1,7 @@
-print("this is the server")
+import subprocess
+
+print("This is the server.")
+
+DH_server = subprocess.run(["go run DH_server/DH_server.go DH_server/server_main.go"], capture_output=True, text=True)
+
+print(DH_server.stdout)
