be21c89ec031551b2e34311f0403c3b28e856748
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-04-24 23:35:35 -0500

    server.py can now use DH_server go code

--- server.py
+++ server.py
@@ -2,6 +2,6 @@ import subprocess
 
 print("This is the server.")
 
-DH_server = subprocess.run(["go run DH_server/DH_server.go DH_server/server_main.go"], capture_output=True, text=True)
+DH_server = subprocess.run(["go", "run", "DH_server/DH_server.go", "DH_server/server_main.go"], capture_output=True, text=True)
 
 print(DH_server.stdout)
